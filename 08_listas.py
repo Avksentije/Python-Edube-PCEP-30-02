04a1680f3f2644eb00893396f57617e4887d782d
Tania <a.klozevits@gmail.com>
2024-03-06 15:22:10 -0600

    commit

--- 08_listas.py
+++ 08_listas.py
@@ -1,12 +1,11 @@
-user_word = input("¿Cuál es tu palabra favorita?")
+user_word = input("¿Cuál es tu palabra favorita? ")
 user_word = user_word.upper()
 
 for letter in user_word:
     # Completa el cuerpo del bucle for.
-
         
     if letter in ["A", "E", "I", "O", "U"]:
         continue
-    
+
     else: 
         print(letter)
