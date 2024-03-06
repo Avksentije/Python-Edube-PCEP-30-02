e03c39bc4e780a752d45aa7f1e0ff1b1f45f62b2
Tania <a.klozevits@gmail.com>
2024-03-06 16:09:02 -0600

    commit

--- 23_listas.py
+++ 23_listas.py
@@ -10,4 +10,15 @@ print("Mis números favoritos son: ", numbers[2], "y ", numbers[1])
 numbers[2] = "voy a cambiar este número"
 print(numbers)
 numbers[2] = 17
-print(numbers) # :))
+print("Nueva lista: ", numbers)
+print("\n")
+
+
+list = [10, 5, 7, 2, 1]
+print("Contenido de la lista original:", list)  # Imprimiendo contenido de la lista original.
+
+list[0] = 111
+print("\nPrevio contenido de la lista:", list)  # Imprimiendo contenido de la lista anterior.
+
+list[1] = list[4]  # Copiando el valor del quinto elemento al segundo elemento.
+print("\nNuevo contenido de la lista:", list)  # Imprimiendo el contenido de la lista actual.
