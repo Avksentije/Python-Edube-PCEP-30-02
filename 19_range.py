77c02bcb5c35f6028ce9b544845a85a8d87c21d5
Tania <a.klozevits@gmail.com>
2024-03-05 23:35:59 -0600

    initial commit

---
+++ 19_range.py
@@ -0,0 +1,10 @@
+n = range(4)
+
+for num in n:
+    print(num - 1)
+else:
+    print(num)
+
+
+for i in range(0, 6, 3):
+    print(i)
---
+++ 20_bits_desplazamiento.py
@@ -0,0 +1,4 @@
+var = 17
+var_right = var >> 1
+var_left = var << 2
+print(var, var_left, var_right)
