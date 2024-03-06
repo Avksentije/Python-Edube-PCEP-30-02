810028f12003185abd5396f7e6ceb6f5e03eb38c
Tania <a.klozevits@gmail.com>
2024-03-06 15:18:54 -0600

    commit

---
+++ 23_listas.py
@@ -0,0 +1,13 @@
+numbers = [10, 5, 7, 2, 1]
+
+print(numbers)
+print(numbers[2])
+
+print(len(numbers))
+print("Mis números favoritos son: ", numbers[2], "y ", numbers[1])
+
+
+numbers[2] = "voy a cambiar este número"
+print(numbers)
+numbers[2] = 17
+print(numbers) # :))
