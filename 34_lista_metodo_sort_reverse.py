c10b0a30899995c0f38111ecb524a493630d0d68
Tania <a.klozevits@gmail.com>
2024-03-06 23:46:07 -0600

    commit

---
+++ 34_lista_metodo_sort_reverse.py
@@ -0,0 +1,28 @@
+#EJ 1
+my_list = [8, 10, 6, 2, 4]
+my_list.sort()
+print(my_list)
+
+
+#EJ 2
+lst = [5, 3, 1, 2, 4]
+print(lst)
+
+lst.reverse()
+print(lst)  # salida: [4, 2, 1, 3, 5]
+
+
+#EJ 3
+lst = ["D", "F", "A", "Z"]
+lst.sort()
+print(lst)
+
+#EJ 4
+a = 3
+b = 1
+c = 2
+
+lst = [a, c, b]
+lst.sort()
+
+print(lst)
