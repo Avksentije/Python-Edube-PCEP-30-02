3ce39c3feba9ee5c3bf2f2f44ec009ec931940d9
Tania <a.klozevits@gmail.com>
2024-03-08 19:41:50 -0600

    commit

--- 03_Funciones/47_definicion.py
+++ 03_Funciones/47_definicion.py
@@ -4,3 +4,12 @@ def message():
 print("Se comienza aquí.")
 message()
 print("Se termina aquí.")
+
+
+def hello(name):    # definiendo una función
+    print("Hola,", name)    # cuerpo de la función
+
+
+name = input("Ingresa tu nombre: ")
+
+hello(name)    # invocación de la función
