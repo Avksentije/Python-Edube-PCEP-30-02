c75ff2391efa5f4d20342dc620fac9e6463b5757
Tania <a.klozevits@gmail.com>
2024-03-10 11:08:28 -0600

    commit

---
+++ 03_Funciones/49_argumentos_palabra_clave.py
@@ -0,0 +1,5 @@
+def presentacion(nombre, apellido):
+    print("Hola, mi nombre es", nombre, apellido)
+
+presentacion(nombre="Tania", apellido="RC")
+presentacion(apellido="MP", nombre="Amrita")
---
+++ 03_Funciones/50_invocacion_de_funciones.py
@@ -0,0 +1,12 @@
+def adding(a, b, c):
+    print(a, "+", b, "+", c, "=", a + b + c)
+
+adding(a="pizza", b="palomitas", c="pelis")
+print("La tarde perfecta ;)")
+
+adding("hojas", "mar", "sal") #imprime el resultado de la función
+
+adding(1, 2, 3) #imprime el resultado de la función
+
+adding(a=1, b=13, c=12E3)
+print(adding) #ojo: imprime la función en sí (nombre, tipo y ubicacion en la memoria) y no el resultado de la función
