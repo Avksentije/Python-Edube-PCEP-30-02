bd551c05c0340934e79ebec216414ca6ce04ba06
Tania <a.klozevits@gmail.com>
2024-03-11 16:02:22 -0600

    commit

--- 03_Funciones/60_funcion_numero_primo.py
+++ 03_Funciones/60_funcion_numero_primo.py
@@ -7,10 +7,13 @@ def is_prime(num):
         return False    
 #Para determinar si un número a partir del 3 es primo, 
 #necesitamos verificar si es divisible por cualquier número primo menor o igual a su raíz cuadrada.
-    for i in range(3, int(num**0.5), 2): #Revisamos solo numeros impares a partir del 3, dado que los pares no son primos.
+    for i in range(3, int(num**0.5), 2): 
         if num % i == 0: 
             return False
     return True
+#Para este último for, tuvimos que investigar los principios para evaluar un número primo
+#La forma de resolverlo es revisar solo numeros impares a partir del 3, dado que los pares no son primos.
+#Para esto, se emplea el criterio de dividir entre 2 y entre cualquier otro número primo (raíz cuadrada)
 #Al aplicar int() a un número flotante, la función int() truncará el número flotante hacia cero, eliminando cualquier parte fraccionaria y dejando solo la parte entera.
 
 for i in range(1, 20):
