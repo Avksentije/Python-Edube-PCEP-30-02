b7ad04d2b20e8faf652bb3ade436e4271df051a2
Tania <a.klozevits@gmail.com>
2024-03-21 14:57:59 -0600

    commit

--- 04_Tuplas/75_tuplas_instrucciones.py
+++ 04_Tuplas/75_tuplas_instrucciones.py
@@ -14,3 +14,33 @@ for elem in my_tuple:
 my_tuple.append(10000)
 del my_tuple[0]
 my_tuple[1] = -10
+
+#Otras propiedades de las tuplas
+#Indexación:
+my_tuple = (1, 2.0, "cadena", [3, 4], (5, ), True)
+print(my_tuple[3])    # salida: [3, 4]
+
+#Inmutabilidad
+my_tuple = (1, 2.0, "cadena", [3, 4], (5, ), True)
+my_tuple[2] = "guitarra"    # La excepción TypeError será lanzada.
+
+#Eliminar una tupla completa
+my_tuple = 1, 2, 3, 
+del my_tuple
+print(my_tuple)    # NameError: name 'my_tuple' is not defined
+
+#Crear tuplas completas con la función integrada de Python tuple()
+my_tuple = tuple((1, 2, "cadena"))
+print(my_tuple)
+
+my_list = [2, 4, 6]
+print(my_list)    # salida: [2, 4, 6]
+print(type(my_list))    # salida: <class 'list'>
+
+tup = tuple(my_list)
+print(tup)    # salida: (2, 4, 6)
+print(type(tup))    # salida: <class 'tuple'> #acá convertimos una lista en una tupla :))
+
+tup = 1, 2, 3, 
+my_list = list(tup)
+print(type(my_list))    # salida: <class 'list'> #y acá lo hicimos a la inversa :))
